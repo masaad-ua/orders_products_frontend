f993beab70ee8e099e82f10141a4838605147df1
maksymosypchuk <maksym3d@gmail.com>
2026-07-18 16:08:04 +0300

    add (classNames.ts, classNames.test.ts, TopMenu.module.scss, TopMenu.module.scss), update ( App.tsx , main.tsx ): add bootstrap style,

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+
       <div>Hello world!</div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components')
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  }
 })

e6a60c90452fd54c81d0fbd1d241bdf2b72f2952
maksymosypchuk <maksym3d@gmail.com>
2026-07-19 12:02:53 +0300

    update ( TopMenu.tsx, TopMenu.module.scss, variables.scss ): update layout, add new variables

--- src/widgets/TopMenu/ui/TopMenu.tsx
+++ src/widgets/TopMenu/ui/TopMenu.tsx
@@ -1,8 +1,9 @@
-import {useTranslation} from 'react-i18next';
-import {classNames} from '@/shared/lib/classNames/classNames';
+//import {useTranslation} from 'react-i18next';
 import cls from './TopMenu.module.scss';
-import './TopMenu.css';
-import {memo} from "react"
+import './TopMenu.module.scss';
+import  {memo} from "react"
+import {classNames} from "@/shared/lib/classNames/classNames.ts";
+import logo from '@/assets/images/logo.png';
 
 
 interface TopMenuProps {
@@ -10,12 +11,54 @@ interface TopMenuProps {
 }
 
 export const TopMenu = memo((props: TopMenuProps) => {
-    const {className} = props;
-    const {t} = useTranslation();
+    // const {className} = props;
+    // const {t} = useTranslation();
 
     return (
-        <header className={}>
+        <header className={cls.header}>
+            <div className={"container-fluid"}>
+                <div className={ classNames(cls.header__content, {}, ["d-flex align-items-center justify-content-between"])}>
 
+                    <div className={"d-flex align-items-center"}>
+                        <div className={classNames(cls.header__logo)}>
+                            <img
+                                src={logo}
+                                alt="Inventory"
+                                className={cls.header__logoImage}
+                            />
+                            <h2 className={cls.header__title}>INVENTORY</h2>
+                        </div>
+                        <div className={classNames(cls.header__search, {}, [`ms-5`])}>
+                            <input
+                                type="text"
+                                className={`form-control ${cls.header__searchInput}`}
+                                placeholder="Поиск"
+                            />
+                        </div>
+                    </div>
+
+                    <div className={"d-flex align-items-end"}>
+                        <p className={classNames(cls.header__day)}>
+                            Вторник
+                        </p>
+                        <div className={classNames(cls.header__dateTime)}>
+
+                            <span className={classNames(cls.header__date)}>
+                                06 Apr, 2017
+                            </span>
+                            <i
+                                className={`bi bi-clock ${cls.header__clock}`}
+                            />
+
+                            <span className={cls.header__time}>
+                                17:20
+                            </span>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </header>
     );
 });

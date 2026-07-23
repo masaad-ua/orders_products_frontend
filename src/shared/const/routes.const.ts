c68907ddcbf9dc03fc969b73f5f0c1fd9d851b2c
maksymosypchuk <maksym3d@gmail.com>
2026-07-23 11:09:18 +0300

    update ( SideBar.tsx, SideBar.module.scss ) : update layout

--- src/shared/const/routes.const.ts
+++ src/shared/const/routes.const.ts
@@ -1,4 +1,7 @@
 export const ROUTES = {
     ORDERS: "/orders",
+    GROUPS: "/groups",
     PRODUCTS: '/products',
+    USERS: '/users',
+    SETTINGS: "/settings"
 }
--- src/widgets/SideBar/ui/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar.tsx
@@ -1,7 +1,11 @@
 import {useTranslation} from 'react-i18next';
 import {classNames} from '@/shared/lib/classNames/classNames';
 import cls from './SideBar.module.scss';
-import {memo} from "react"
+import {memo, useEffect, useState} from "react"
+import portrait from '@/assets/images/portrait.png';
+import {Settings} from "lucide-react";
+import {Link, NavLink, useLocation} from "react-router-dom";
+import {ROUTES} from "@/shared/const/routes.const.ts";
 
 
 interface SideBarProps {
@@ -13,11 +17,83 @@ export const SideBar = memo((props: SideBarProps) => {
     const {t} = useTranslation();
 
     return (
-        <aside className={classNames(cls.SideBar, {}, [className])}>
-            <div className={"sidebar__logoWrapper"}>
+        <aside className={classNames(className || "")}>
+            <div className={classNames(cls.sidebar,
+                {},
+                ["d-flex", "flex-column", "justify-content-start", "align-items-center"])}
+            >
+                <div className={cls.sidebar__avatarWrapper}>
+                    <img src={portrait} alt="" className={cls.sidebar__avatar}/>
+                    <div className={classNames(cls.sidebar__settings,
+                        {},
+                        ["d-flex", "justify-content-center", "align-items-center"])}>
+                        <Settings fill="red"  className={cls.sidebar__settingsIcon} />
+                    </div>
+                </div>
+                <ul className={cls.sidebar__list}>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                classNames(cls.sidebar__listLink, {
+                                    [cls.active]: isActive,
+                                })
+                            }
+                            to={ROUTES.ORDERS}>
+                            {t("SIDEBAR.ORDERS")}
+                        <span className={cls.sidebar__listLinkLine}></span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                classNames(cls.sidebar__listLink, {
+                                    [cls.active]: isActive,
+                                })
+                            }
+                            to={ROUTES.GROUPS}>
+                            {t("SIDEBAR.GROUPS")}
+                            <span className={cls.sidebar__listLinkLine}></span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                classNames(cls.sidebar__listLink, {
+                                    [cls.active]: isActive,
+                                })
+                            }
+                            to={ROUTES.PRODUCTS}>
+                            {t("SIDEBAR.PRODUCTS")}
+                            <span className={cls.sidebar__listLinkLine}></span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                classNames(cls.sidebar__listLink, {
+                                    [cls.active]: isActive,
+                                })
+                            }
+                            to={ROUTES.USERS}>
+                            {t("SIDEBAR.USERS")}
+                            <span className={cls.sidebar__listLinkLine}></span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                classNames(cls.sidebar__listLink, {
+                                    [cls.active]: isActive,
+                                })
+                            }
+                            to={ROUTES.SETTINGS}>
+                            {t("SIDEBAR.SETTINGS")}
+                            <span className={cls.sidebar__listLinkLine}></span>
+                        </NavLink>
+                    </li>
+                </ul>
 
             </div>
-
         </aside>
     );
 });

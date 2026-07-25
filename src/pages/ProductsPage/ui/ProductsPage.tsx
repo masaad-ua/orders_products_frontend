84fa87f2497bc72af0a5d1f43367f2be586ab6b7
maksymosypchuk <maksym3d@gmail.com>
2026-07-25 12:25:49 +0300

    update ( ProductsPage.module.scss, ProductsPage.tsx): update layout add new component ProductList. add (ProductsList.module.scss, ProductList.module.scss, index.ts )

--- src/pages/ProductsPage/ui/ProductsPage.tsx
+++ src/pages/ProductsPage/ui/ProductsPage.tsx
@@ -8,6 +8,7 @@ import Monitor from '@/assets/images/monitor.png';
 import {PriceItem} from "@/shared/ui/PriceItem";
 import {DateItem} from "@/shared/ui/DateItem";
 import {ListItemName} from "@/shared/ui/ListItemName";
+import {ProductsList} from "@/widgets/ProductsList";
 
 interface ProductsPageProps {
     className?: string;
@@ -32,80 +33,7 @@ const ProductsPage = memo((props: ProductsPageProps) => {
                     onSpecificationChange={setSpecification}
                 />
             </div>
-            <ul className={classNames(cls.products_list)}>
-                <li className={classNames(cls.products__listItem,{}, ["d-flex"])}>
-                    <div className={classNames(cls.products__listItemIndicatorWrapper,
-                        {},
-                        ["d-flex", "justify-content-center" ,"align-items-center"] )}>
-                        <div className={classNames(cls.products__listItemIndicator)}></div>
-                    </div>
-
-
-                    <div className={classNames(cls.products__listItemDeviceWrapper,
-                        {},
-                        ["d-flex", "align-items-center"])}>
-                        <img src={Monitor}
-                             alt=""
-                             className={classNames(cls.products__listItemDeviceImage)}/>
-                        <div className={classNames(cls.products__listItemNameDeviceNameWrapper)}>
-                            <p>Gigabyte Technology X58-USB3 (Socket 1366) 6 X58-USB3</p>
-                            <p className={"color_grey font_size_14"}>SN-12.3456789</p>
-                        </div>
-                    </div>
-
-                    <div
-                        className={classNames(cls.products__listItemNameStatusWrapper,
-                            {},
-                            [ "d-flex", "justify-content-center", "align-items-center"]
-                           )}>
-                        <p className={classNames(cls.products__listItemNameStatus,
-                            {
-                                [cls.free]: true
-                            })}>
-                            cвободен
-                        </p>
-                    </div>
-
-
-                    <div className={classNames(cls.products__listItemPeriodWrapper,
-                        {},
-                        ["d-flex", "flex-column", "align-items-start",  "justify-content-center"]
-
-                        )}>
-                        <p>
-                            <span className={"font_size_12"}>с</span>
-                            &nbsp;06 / 04/ 2017
-                        </p>
-                        <p>
-                            <span className={"font_size_12"}>по </span>
-                            &nbsp;06 / 04/ 2017
-                        </p>
-                    </div>
-
-                    <div className={classNames(cls.products__listItemStatus,
-                        {},
-                        ["d-flex", "justify-content-center", "align-items-center"])}>
-                        <p>новый</p>
-                    </div>
-                    <PriceItem
-                        className={cls.products__listItemPriceWrapper}
-                    />
-                    <ListItemName
-                        className={cls.products__listItemTitleGroupWrapper}
-                    />
-
-                    <ListItemName
-                        className={cls.products__listItemNameOfCustomerWrapper}
-                    />
-                    <ListItemName
-                        className={cls.products__listItemNameOfOrdersWrapper}
-                    />
-                    <DateItem
-                        className={cls.products__listItemDateWrapper}
-                    />
-                    <TrashButton/>
-                </li>
-            </ul>
+            <ProductsList/>
         </div>
     )
 });
